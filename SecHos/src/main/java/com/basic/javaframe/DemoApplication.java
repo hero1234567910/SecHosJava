f2eb910cc7d888eae83d8f407347a545694f8dd3
hero12345679 <1039323578@qq.com>
2019-09-12 08:44:00 +0800

    修改配置

--- SecHos/src/main/java/com/basic/javaframe/DemoApplication.java
+++ SecHos/src/main/java/com/basic/javaframe/DemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import javax.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 
 @MapperScan("com.basic.javaframe.dao")
 @SpringBootApplication
@@ -14,5 +17,5 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	
 }
